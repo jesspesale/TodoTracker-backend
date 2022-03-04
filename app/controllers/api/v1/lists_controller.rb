f1f9c2cabd800860961c9a212d85fb898463c1c8
jesspesale <jess_pesale@aol.com>
2022-03-03 21:00:08 -0500

    added index and create action in list controller

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,2 +1,32 @@
 class API::V1::ListsController < ApplicationController
+
+    def index
+        @lists = List.all 
+        render json: @lists
+    end
+
+    def create
+        @list = List.create(list_params)
+
+        if @list.save
+            render json: @list
+        else
+            render json: {error: "Error creating the list"}
+        end
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def list_params
+        params.require(:list).permit(:title, :completed)
+    end
+
 end
