0e3a2bf5045409fc380d3d40aa9c6919984b780c
jesspesale <jess_pesale@aol.com>
2022-03-03 21:10:08 -0500

    added index action to get all list items associated with that list

--- app/controllers/api/v1/list_items_controller.rb
+++ app/controllers/api/v1/list_items_controller.rb
@@ -3,12 +3,18 @@ class Api::V1::ListItemsController < ApplicationController
     before_action :set_list
 
     def index
-        @list_items = ListItem.all
+        @list_items = @list.list_items
         render json: @list_items
     end
 
     def create
+        @list_item = ListItem.create(list_item_params)
 
+        if @list_item.save
+            render json: @list_item
+        else
+            render json: {error: "Error creating list item"}
+        end
     end
 
     def show
