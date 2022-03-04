4470b38149b9eceb3bbbbe43eb7ebb54855e4b88
jesspesale <jess_pesale@aol.com>
2022-03-03 21:07:38 -0500

    added helper method for list itmes to alwasy find and set the list that that item is for

--- app/controllers/api/v1/list_items_controller.rb
+++ app/controllers/api/v1/list_items_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::ListItemsController < ApplicationController
+
+    before_action :set_list
+
+    def index
+        @list_items = ListItem.all
+        render json: @list_items
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def set_list
+        @list = List.find(params[:list_id])
+    end
+
+    def list_item_params
+        params.require(:list_item).permit(:description, :completed, :list_id)
+    end
+
 end
