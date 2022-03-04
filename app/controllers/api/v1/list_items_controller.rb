b1829937180f71c769cd6d3e8bdd0fbf3eaae7be
jesspesale <jess_pesale@aol.com>
2022-03-03 21:24:19 -0500

    added show, create and destroy action for list_items controller

--- app/controllers/api/v1/list_items_controller.rb
+++ app/controllers/api/v1/list_items_controller.rb
@@ -7,8 +7,16 @@ class Api::V1::ListItemsController < ApplicationController
         render json: @list_items
     end
 
+    
+    def show
+    #    @list_item = @list.list_items.find_by(id: params[:id])
+       
+        @list_item = ListItem.find(params[:id])
+        render json: @list_item
+    end
+    
     def create
-        @list_item = ListItem.create(list_item_params)
+        @list_item = @list.list_items.new(list_item_params)
 
         if @list_item.save
             render json: @list_item
@@ -17,12 +25,9 @@ class Api::V1::ListItemsController < ApplicationController
         end
     end
 
-    def show
-
-    end
-
     def destroy
-
+        @list_item = ListItem.find(params[:id])
+        @list_item.destroy
     end
 
     private
